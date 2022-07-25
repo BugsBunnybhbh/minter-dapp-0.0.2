7944e36c4f5fae7fec620be04210131d5bf4da42
BugsBunnybhbh <102867317+BugsBunnybhbh@users.noreply.github.com>
2022-07-25 10:19:54 +0200

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = [0xaA7018104bbD6C4C4f74A3ba401779F7D7C9e0d1]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "Y0xaA7018104bbD6C4C4f74A3ba401779F7D7C9e0d1S"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x943F381B0DFA7a0Bf37e526F2e64924608fdF81B"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
